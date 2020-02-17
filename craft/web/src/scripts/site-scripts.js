758df499c93a9d7197e131fb0a7111364b5ac70e
Kelly <kellyannmcnamara01@gmail.com>
2020-02-17 13:02:28 -0500

    moving accordion scrip to site scripts file

--- craft/web/src/scripts/site-scripts.js
+++ craft/web/src/scripts/site-scripts.js
@@ -1,3 +1,22 @@
+// table of context
+// -- accordion
+
+
 window.addEventListener("load", function(){
 
+	// -- accordion
+	$('.accordion__trigger').click(function(){
+		// grabbing accordion controls and id + the accordion's panel
+		var ariaControls = $(this).attr('aria-controls'),
+			accordionId = $(this).attr('id'),
+			accordionPanel = $('.accordion-panel[id="' + ariaControls + '"]');
+
+		// setting all accordions to expanded false expect the one clicked
+		$('.accordion__trigger').attr('aria-expanded', false);
+		$(this).attr('aria-expanded', true);
+
+		// setting all panels to hidden true expect the one clicked
+		$('.accordion-panel').attr('hidden', true);
+		accordionPanel.attr('hidden', false);
+	});
 });
